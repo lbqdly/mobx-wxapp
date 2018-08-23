628088881086c7ac04f173a0d476eae570a1c7aa
liangyun <aaron.li@wetax.com.cn>
2018-08-23 10:36:06 +0800

    setData delay

--- utils/mobx-wxapp.js
+++ utils/mobx-wxapp.js
@@ -1,40 +1,38 @@
-import { autorun, toJS, isObservableObject } from './mobx';
+import { autorun, toJS, isObservableObject } from "./mobx";
 
 /**
  * decorator
  * @param {Object} props
  */
 function observer(props) {
-  if (typeof props !== 'object') {
-    throw new Error('The props must be a Object');
+  if (typeof props !== "object") {
+    throw new Error("The props must be a Object");
   }
   return function(page) {
     const _onLoad = page.onLoad;
     const _onUnload = page.onUnload;
-    let timer = null;
-    // production tested, 50ms is the perfect delay
     const DELAY = 50;
     page.onLoad = function() {
-      this._update = autorun(() => {
-        let data = {};
-        Object.keys(props).forEach(key => {
-          let prop = props[key];
-          if (!isObservableObject(prop)) {
-            throw new Error('The props must be a ObservableObject');
-          }
-          data[key] = {};
-          let displayKeys = Object.getOwnPropertyNames(prop).filter(
-            key => key !== '$mobx' && typeof prop[key] !== 'function'
-          );
-          displayKeys.forEach(k => {
-            data[key][k] = toJS(prop[k]);
+      this._update = autorun(
+        () => {
+          let data = {};
+          Object.keys(props).forEach(key => {
+            let prop = props[key];
+            if (!isObservableObject(prop)) {
+              throw new Error("The props must be a ObservableObject");
+            }
+            data[key] = {};
+            let displayKeys = Object.getOwnPropertyNames(prop).filter(
+              key => key !== "$mobx" && typeof prop[key] !== "function"
+            );
+            displayKeys.forEach(k => {
+              data[key][k] = toJS(prop[k]);
+            });
           });
-        });
-        clearTimeout(timer);
-        timer = setTimeout(() => {
           this.setData(data);
-        }, DELAY);
-      });
+        },
+        { delay: DELAY }
+      );
       if (_onLoad) {
         _onLoad.apply(this, arguments);
       }
